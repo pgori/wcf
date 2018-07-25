79c9fc1a3ad1ba189feab4ee1dfc0883319ea25c
Pietro Gori <pietro_gori@hotmail.com>
2018-07-24 23:13:53 -0300

    anotações da lib ServiceModel e Runtime.Serialization

---
+++ Passagens/ClienteDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    public class ClienteDao
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public void Add(Cliente cliente)
+        {
+            ClienteDao.clientes.Add(cliente);
+        }
+        public Cliente Buscar(string nome)
+        {
+            var resultado = ClienteDao.clientes.Where(c => c.Nome.Equals(nome)).FirstOrDefault();
+            return resultado;
+        }
+    }
+}
---
+++ Passagens/ClienteService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens
+{
+    public class ClienteService : IClienteService
+    {
+        public void Add(Cliente cliente)
+        {
+            ClienteDao clienteDao = new ClienteDao();
+            clienteDao.Add(cliente);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            ClienteDao clienteDao = new ClienteDao();
+            return clienteDao.Buscar(nome);
+        }
+    }
+}
